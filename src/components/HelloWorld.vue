553cbf285495bfad2ccf23c0ebaa68f5ca86d94c
ranup <2658981151@qq.com>
2024-12-03 11:07:15 +0800

    fix:修改介绍文字

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
     <div class="greetings">
         <h1 class="green">{{ msg }}</h1>
-        <h3>Student Information Management App!</h3>
+        <h3>Student Performance Management App!</h3>
     </div>
 </template>
 
