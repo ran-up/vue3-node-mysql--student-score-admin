25e9bb551b7ad66495cb032e2faf7f5acbb1b413
ranup <2658981151@qq.com>
2024-12-03 11:08:39 +0800

    fix:修改介绍文字

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,42 +1,45 @@
 <template>
     <div>
-        <h3>{{ msg }}</h3>
-        <h4>Student Scores Table</h4>
-        <table>
-            <tr>
-                <th>No</th>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Gender</th>
-                <th>Chinese</th>
-                <th>Math</th>
-                <th>English</th>
-                <th>Admin</th>
-            </tr>
-            <tr v-for="(s, index) in info" :key="index">
-                <td>{{ index + 1 }}</td>
-                <td>{{ s.id }}</td>
-                <td>{{ s.name }}</td>
-                <td>{{ s.gender }}</td>
-                <td>{{ s.chinese }}</td>
-                <td>{{ s.math }}</td>
-                <td>{{ s.english }}</td>
-                <td>
-                    <router-link to="/insert" active-class="active">Insert</router-link>&nbsp;&nbsp;
-                    <router-link :to="'/edit/' + s.id" active-class="active">Edit</router-link
-                    >&nbsp;&nbsp;
-                    <a href @click="del_stu_info(s.id)" active-class="active">Del</a>&nbsp;&nbsp;
-                </td>
-            </tr>
-        </table>
+        <welcome-item>
+            <template #icon>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" fill="currentColor">
+                    <path
+                        d="M11 2.253a1 1 0 1 0-2 0h2zm-2 13a1 1 0 1 0 2 0H9zm.447-12.167a1 1 0 1 0 1.107-1.666L9.447 3.086zM1 2.253L.447 1.42A1 1 0 0 0 0 2.253h1zm0 13H0a1 1 0 0 0 1.553.833L1 15.253zm8.447.833a1 1 0 1 0 1.107-1.666l-1.107 1.666zm0-14.666a1 1 0 1 0 1.107 1.666L9.447 1.42zM19 2.253h1a1 1 0 0 0-.447-.833L19 2.253zm0 13l-.553.833A1 1 0 0 0 20 15.253h-1zm-9.553-.833a1 1 0 1 0 1.107 1.666L9.447 14.42zM9 2.253v13h2v-13H9zm1.553-.833C9.203.523 7.42 0 5.5 0v2c1.572 0 2.961.431 3.947 1.086l1.107-1.666zM5.5 0C3.58 0 1.797.523.447 1.42l1.107 1.666C2.539 2.431 3.928 2 5.5 2V0zM0 2.253v13h2v-13H0zm1.553 13.833C2.539 15.431 3.928 15 5.5 15v-2c-1.92 0-3.703.523-5.053 1.42l1.107 1.666zM5.5 15c1.572 0 2.961.431 3.947 1.086l1.107-1.666C9.203 13.523 7.42 13 5.5 13v2zm5.053-11.914C11.539 2.431 12.928 2 14.5 2V0c-1.92 0-3.703.523-5.053 1.42l1.107 1.666zM14.5 2c1.573 0 2.961.431 3.947 1.086l1.107-1.666C18.203.523 16.421 0 14.5 0v2zm3.5.253v13h2v-13h-2zm1.553 12.167C18.203 13.523 16.421 13 14.5 13v2c1.573 0 2.961.431 3.947 1.086l1.107-1.666zM14.5 13c-1.92 0-3.703.523-5.053 1.42l1.107 1.666C11.539 15.431 12.928 15 14.5 15v-2z" />
+                </svg>
+            </template>
+            <template #heading>说明文档</template>
+            <p>学生成绩管理系统基于 Vue.js 框架开发</p>
+        </welcome-item>
+        <welcome-item>
+            <template #icon>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="currentColor">
+                    <path
+                        d="M11.447 8.894a1 1 0 1 0-.894-1.789l.894 1.789zm-2.894-.789a1 1 0 1 0 .894 1.789l-.894-1.789zm0 1.789a1 1 0 1 0 .894-1.789l-.894 1.789zM7.447 7.106a1 1 0 1 0-.894 1.789l.894-1.789zM10 9a1 1 0 1 0-2 0h2zm-2 2.5a1 1 0 1 0 2 0H8zm9.447-5.606a1 1 0 1 0-.894-1.789l.894 1.789zm-2.894-.789a1 1 0 1 0 .894 1.789l-.894-1.789zm2 .789a1 1 0 1 0 .894-1.789l-.894 1.789zm-1.106-2.789a1 1 0 1 0-.894 1.789l.894-1.789zM18 5a1 1 0 1 0-2 0h2zm-2 2.5a1 1 0 1 0 2 0h-2zm-5.447-4.606a1 1 0 1 0 .894-1.789l-.894 1.789zM9 1l.447-.894a1 1 0 0 0-.894 0L9 1zm-2.447.106a1 1 0 1 0 .894 1.789l-.894-1.789zm-6 3a1 1 0 1 0 .894 1.789L.553 4.106zm2.894.789a1 1 0 1 0-.894-1.789l.894 1.789zm-2-.789a1 1 0 1 0-.894 1.789l.894-1.789zm1.106 2.789a1 1 0 1 0 .894-1.789l-.894 1.789zM2 5a1 1 0 1 0-2 0h2zM0 7.5a1 1 0 1 0 2 0H0zm8.553 12.394a1 1 0 1 0 .894-1.789l-.894 1.789zm-1.106-2.789a1 1 0 1 0-.894 1.789l.894-1.789zm1.106 1a1 1 0 1 0 .894 1.789l-.894-1.789zm2.894.789a1 1 0 1 0-.894-1.789l.894 1.789zM8 19a1 1 0 1 0 2 0H8zm2-2.5a1 1 0 1 0-2 0h2zm-7.447.394a1 1 0 1 0 .894-1.789l-.894 1.789zM1 15H0a1 1 0 0 0 .553.894L1 15zm1-2.5a1 1 0 1 0-2 0h2zm12.553 2.606a1 1 0 1 0 .894 1.789l-.894-1.789zM17 15l.447.894A1 1 0 0 0 18 15h-1zm1-2.5a1 1 0 1 0-2 0h2zm-7.447-5.394l-2 1 .894 1.789 2-1-.894-1.789zm-1.106 1l-2-1-.894 1.789 2 1 .894-1.789zM8 9v2.5h2V9H8zm8.553-4.894l-2 1 .894 1.789 2-1-.894-1.789zm.894 0l-2-1-.894 1.789 2 1 .894-1.789zM16 5v2.5h2V5h-2zm-4.553-3.894l-2-1-.894 1.789 2 1 .894-1.789zm-2.894-1l-2 1 .894 1.789 2-1L8.553.106zM1.447 5.894l2-1-.894-1.789-2 1 .894 1.789zm-.894 0l2 1 .894-1.789-2-1-.894 1.789zM0 5v2.5h2V5H0zm9.447 13.106l-2-1-.894 1.789 2 1 .894-1.789zm0 1.789l2-1-.894-1.789-2 1 .894 1.789zM10 19v-2.5H8V19h2zm-6.553-3.894l-2-1-.894 1.789 2 1 .894-1.789zM2 15v-2.5H0V15h2zm13.447 1.894l2-1-.894-1.789-2 1 .894 1.789zM18 15v-2.5h-2V15h2z" />
+                </svg>
+            </template>
+            <template #heading>技术介绍</template>
+            <p>使用 Vue3 + Vue-router + Sass + Axios + Node + Express + Mysql 协同开发</p>
+        </welcome-item>
+        <welcome-item>
+            <template #icon>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor">
+                    <path
+                        d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.666.105 5.5 5.5 0 0 0-.114 7.665L10 18.78l8.39-8.4a5.5 5.5 0 0 0-.114-7.665 5.5 5.5 0 0 0-7.666-.105l-.61.61z" />
+                </svg>
+            </template>
+            <template #heading>功能介绍</template>
+            <p>学生成绩浏览、新增、编辑和删除</p>
+        </welcome-item>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import WelcomeItem from '@/components/WelcomeItem.vue'
+</script>
 <script>
 export default {
     name: 'StudentHome'
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
