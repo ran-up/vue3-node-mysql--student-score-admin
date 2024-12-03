b6500de91b1e2c77c2349d9e5e7dbd2687ad2a96
ranup <2658981151@qq.com>
2024-12-03 23:56:56 +0800

    feat:新增选手api开发

--- src/server/api.js
+++ src/server/api.js
@@ -3,8 +3,38 @@ exports.all = (req, res) => {
     const sql = 'select * from stu_info'
     db.query(sql, (err, data) => {
         if (err) {
-            return res.send('错误：%s', err.message)
+            return res.send({
+                code: 400,
+                message: err.message
+            })
         }
-        res.send(data)
+        res.send({
+            code: 200,
+            message: '查询成功',
+            data
+        })
+    })
+}
+
+exports.add = (req, res) => {
+    const sql = 'insert into stu_info (name, gender, chinese, math, english) value (?, ?, ?, ?, ?)'
+    const { name, gender, chinese, math, english } = req.body
+    db.query(sql, [name, gender, chinese, math, english], (err, data) => {
+        if (err) {
+            return res.send({
+                code: 400,
+                message: err.message
+            })
+        } else if (data.affectedRows > 0) {
+            return res.send({
+                code: 200,
+                message: `添加 ${name} 信息成功`
+            })
+        } else {
+            return res.send({
+                code: 202,
+                message: `${name} 信息已存在`
+            })
+        }
     })
 }
