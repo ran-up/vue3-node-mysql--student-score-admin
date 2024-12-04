252691a02ff5ecd948f9db715ad6cd8c883007d9
ranup <2658981151@qq.com>
2024-12-04 21:08:19 +0800

    update:完成查询与修改api

--- src/server/api.js
+++ src/server/api.js
@@ -1,4 +1,8 @@
 const db = require('./db/mysql.js')
+let stuInfo = {} // 编辑的选手信息
+let hasChanged = false
+
+// 获取所有学生成绩信息
 exports.all = (req, res) => {
     const sql = 'select * from stu_info'
     db.query(sql, (err, data) => {
@@ -15,7 +19,7 @@ exports.all = (req, res) => {
         })
     })
 }
-
+// 添加学生成绩信息
 exports.add = (req, res) => {
     const sql = 'insert into stu_info (name, gender, chinese, math, english) value (?, ?, ?, ?, ?)'
     const { name, gender, chinese, math, english } = req.body
@@ -34,6 +38,89 @@ exports.add = (req, res) => {
             return res.send({
                 code: 202,
                 message: `${name} 信息已存在`
+            })
+        }
+    })
+}
+// 删除学生成绩信息
+exports.del = (req, res) => {
+    const sql = 'delete from stu_info where id = ?'
+    db.query(sql, [req.query.id], (err, data) => {
+        if (err) {
+            return res.send({
+                code: 400,
+                message: '删除失败'
+            })
+        } else if (data.affectedRows > 0) {
+            return res.send({
+                code: 200,
+                message: '删除成功'
+            })
+        } else {
+            return res.send({
+                code: 202,
+                message: '查无此人'
+            })
+        }
+    })
+}
+// 查询单个学生成绩信息
+exports.find = (req, res) => {
+    const sql = 'select * from stu_info where id = ?'
+    db.query(sql, [req.query.id], (err, data) => {
+        if (err) {
+            return res.send({
+                code: 400,
+                message: '查询失败'
+            })
+        } else if (data.length > 0) {
+            stuInfo = data[0]
+            return res.send({
+                code: 200,
+                message: '查询成功',
+                data
+            })
+        } else {
+            return res.send({
+                code: 202,
+                message: '查询失败'
+            })
+        }
+    })
+}
+// 更新学生成绩信息
+exports.update = (req, res) => {
+    hasChanged = false
+    for (const key in req.body) {
+        if (req.body[key] != stuInfo[key]) {
+            hasChanged = true
+        }
+    }
+    if (!hasChanged) {
+        return res.send({
+            code: 202,
+            message: '信息未修改'
+        })
+    }
+    const sql =
+        'update stu_info set name = ?, gender = ?, chinese = ?, math = ?, english = ? where id = ?'
+    const { name, gender, chinese, math, english, id } = req.body
+    db.query(sql, [name, gender, chinese, math, english, id], (err, data) => {
+        if (err) {
+            return res.send({
+                code: 400,
+                message: '信息更新失败'
+            })
+        } else if (data.affectedRows > 0) {
+            return res.send({
+                code: 200,
+                message: '信息更新成功',
+                data
+            })
+        } else {
+            return res.send({
+                code: 202,
+                message: '信息未修改'
             })
         }
     })
