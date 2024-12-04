35dba40c7bff01c5f0804dc0b92914abc3e5868b
ranup <2658981151@qq.com>
2024-12-04 21:52:37 +0800

    update:移除动态路由，改用查询字符串

--- src/server/router.js
+++ src/server/router.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const api = require('./api.js')
 
-router.get('/all', api.all)
-router.post('/add', api.add)
+router.get('/all', api.all) // 查询所有学生成绩信息
+router.post('/add', api.add) // 添加学生成绩信息
+router.delete('/del', api.del) // 删除学生成绩信息
+router.get('/find', api.find) // 查询单个学生成绩信息
+router.put('/update', api.update)
 
 module.exports = router
